52cbfd424a0d42b28bd44d6954da457778006aac
wangyuhang.047 <wangyuhang.047@bytedance.com>
2024-10-26 21:51:30 +0800

    存档

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // 更新倒计时时钟
 function updateCountdown() {
   var now = new Date();
-  var targetDate = new Date("2024-6-7 9:00");
+  var targetDate = new Date("2024-6-14 9:00");
   var timeDiff = targetDate.getTime() - now.getTime();
 
   var days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
@@ -10,7 +10,7 @@ function updateCountdown() {
   var seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
 
   var countdownLabelElement = document.getElementById("countdown-label");
-  countdownLabelElement.textContent = "距离高考还剩";
+  countdownLabelElement.textContent = "距离中考还剩";
 
   var countdownElement = document.getElementById("countdown");
   countdownElement.textContent = days + "天 " + hours + "小时 " + minutes + "分钟 " + seconds + "秒";
@@ -23,7 +23,7 @@ setInterval(updateCountdown, 1000);
 document.getElementById("calculate-btn").addEventListener("click", function() {
   var collegeInput = document.getElementById("college-input").value;
   var resultElement = document.createElement("div");
-  resultElement.textContent = "您考入 " + collegeInput + " 的几率为 100%！";
+  resultElement.textContent = "琦琦考入 " + collegeInput + " 的几率为 100%！";
   resultElement.style.color = 'red';
   document.body.appendChild(resultElement);
 });
@@ -40,7 +40,7 @@ function createSnowflake() {
   const randomColor = getRandomColor();
   snowflake.style.backgroundColor = randomColor;
 
-  const slogans = ["高考加油", "金榜题名", "考满分", "耀眼的未来", "考试顺利", "攀登高峰", "梦想成真", "勇往直前", "努力拼搏"]; // Add your slogans here
+  const slogans = ["琦琦中考加油", "金榜题名", "考满分", "耀眼的未来", "考试顺利", "攀登高峰", "梦想成真", "勇往直前", "努力拼搏"]; // Add your slogans here
   snowflake.innerHTML = slogans[Math.floor(Math.random() * slogans.length)];
 
   return snowflake;
